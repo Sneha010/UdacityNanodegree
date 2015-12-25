f0462142f307ad2d05cb2b52793aaaac049e51a2
Sneha <Sneha>
2015-12-25 23:40:09 +0530

    Revised first app with all modifications done, as suggested by reviewer.

--- app/src/main/java/com/udacity/myappportfolio/MainActivity.java
+++ app/src/main/java/com/udacity/myappportfolio/MainActivity.java
@@ -3,12 +3,12 @@ package com.udacity.myappportfolio;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.udacity.myappportfolio.util.MyUtil;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,37 +25,44 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(getResources().getString(R.string.app_name));
     }
 
-    @OnClick(R.id.btn_spotifyStreamer)
-    public void spotifyStreamerClicked(){
-        MyUtil.displayCustomToast(MainActivity.this , getResources().getString(R.string.landing_button1_toast));
+    public void onlandingButtonClick(View v){
+
+        switch (v.getId()){
+            case R.id.btn_spotifyStreamer:
+                displayToast(getResources().getString(R.string.landing_button1_toast));
+                break;
+
+            case R.id.btn_scoresApp:
+                displayToast(getResources().getString(R.string.landing_button2_toast));
+                break;
+
+            case R.id.btn_libraryApp:
+                displayToast(getResources().getString(R.string.landing_button3_toast));
+                break;
+
+            case R.id.btn_buildItBigger:
+                displayToast(getResources().getString(R.string.landing_button4_toast));
+                break;
+
+            case R.id.btn_xyzReader:
+                displayToast(getResources().getString(R.string.landing_button5_toast));
+                break;
+
+
+            case R.id.btn_capstone:
+                displayToast(getResources().getString(R.string.landing_button6_toast));
+                break;
+
+            default:
+
+        }
+
     }
 
-    @OnClick(R.id.btn_scoresApp)
-    public void scoresAppClicked(){
-        MyUtil.displayCustomToast(MainActivity.this , getResources().getString(R.string.landing_button2_toast));
+
+    public void displayToast(String message){
+        MyUtil.displayCustomToast(MainActivity.this , message);
     }
-
-    @OnClick(R.id.btn_libraryApp)
-    public void libraryAppClicked(){
-        MyUtil.displayCustomToast(MainActivity.this , getResources().getString(R.string.landing_button3_toast));
-    }
-
-    @OnClick(R.id.btn_buildItBigger)
-    public void buildItBiggerClicked(){
-        MyUtil.displayCustomToast(MainActivity.this , getResources().getString(R.string.landing_button4_toast));
-    }
-
-    @OnClick(R.id.btn_xyzReader)
-    public void xyzReaderClicked(){
-        MyUtil.displayCustomToast(MainActivity.this , getResources().getString(R.string.landing_button5_toast));
-    }
-
-    @OnClick(R.id.btn_capstone)
-    public void capstoneClicked(){
-        MyUtil.displayCustomToast(MainActivity.this , getResources().getString(R.string.landing_button6_toast));
-    }
-
-
 
 
 }
