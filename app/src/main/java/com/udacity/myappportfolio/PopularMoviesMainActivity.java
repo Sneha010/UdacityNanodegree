db99a9b4ac1e07cd2758c8682b2cabc13d9b8365
Sneha Khadatare <sneha.khadatare@tcs.com>
2016-01-04 19:39:25 +0530

    Set grid Recycler listview

--- app/src/main/java/com/udacity/myappportfolio/PopularMoviesMainActivity.java
+++ app/src/main/java/com/udacity/myappportfolio/PopularMoviesMainActivity.java
@@ -2,12 +2,18 @@ package com.udacity.myappportfolio;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
+import android.widget.RelativeLayout;
 
 import com.udacity.myappportfolio.bean.MovieMainBean;
 import com.udacity.myappportfolio.interfaces.MovieRestAPI;
 import com.udacity.myappportfolio.util.Constants;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import retrofit.Call;
 import retrofit.Callback;
@@ -20,15 +26,50 @@ import retrofit.Retrofit;
  */
 public class PopularMoviesMainActivity extends BaseActivity{
 
+    @Bind(R.id.list)
+    private RecyclerView lv_gridList;
+
+    @Bind(R.id.rl_error)
+    private RelativeLayout rl_error;
+
+    @Bind(R.id.rl_progress)
+    private RelativeLayout rl_progress;
+
+    @Bind(R.id.rl_gridview)
+    private RelativeLayout rl_gridview;
+
+    @Bind(R.id.swipe_container)
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
+    private boolean isRefresh = false;
+    Call<MovieMainBean> call;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popular_movies_main_layout);
         ButterKnife.bind(this);
 
+        mSwipeRefreshLayout.setRefreshing(true);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                isRefresh = true;
+                executeRetrofittask();
+            }
+        });
+
+        setUpGridListView();
         setUpRetrofit();
+        executeRetrofittask();
     }
 
+    public void setUpGridListView(){
+        GridLayoutManager llm = new GridLayoutManager(PopularMoviesMainActivity.this , 2);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        lv_gridList.setLayoutManager(llm);
+    }
 
     public void setUpRetrofit(){
         Retrofit retrofit = new Retrofit.Builder()
@@ -37,8 +78,13 @@ public class PopularMoviesMainActivity extends BaseActivity{
                 .build();
 
         MovieRestAPI myService = retrofit.create(MovieRestAPI.class);
-        Call<MovieMainBean> call = myService.loadMovies("popularity.desc" , "c828d18e73df20d04a0a762f7a126d29" , "1");
+        call = myService.loadMovies("popularity.desc" , "c828d18e73df20d04a0a762f7a126d29" , "1");
 
+
+
+    }
+
+    public void executeRetrofittask(){
         call.enqueue(new Callback<MovieMainBean>() {
             @Override
             public void onResponse(Response<MovieMainBean> response,
@@ -53,7 +99,6 @@ public class PopularMoviesMainActivity extends BaseActivity{
 
             }
         });
-
     }
 
 
