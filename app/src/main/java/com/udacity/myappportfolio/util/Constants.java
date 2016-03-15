661acfe706e6b8cde68300e123dd1571558e2438
Sneha <Sneha>
2016-03-15 20:18:41 +0530

    Some changes

--- app/src/main/java/com/udacity/myappportfolio/util/Constants.java
+++ app/src/main/java/com/udacity/myappportfolio/util/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 
     public static final String BASE_URL = "http://api.themoviedb.org/3/";
     public static final String IMAGE_POSTER_PATH_BASE_URL = "http://image.tmdb.org/t/p/w342/";
-    public static final String API_KEY = "c828d18e73df20d04a0a762f7a126d29";
+    public static final String API_KEY = "[INSERT API KEY HERE]";
     public static final String POPULARITY = "popularity.desc";
     public static final String VOTE_AVERAGE = "vote_average.desc";
     public static final String YOUTUBE_BASE_URL = "http://www.youtube.com/watch?v=";
